4cb19a72d5ebd2cb7250e3ef4de960af8c5f51c2
Kayrom Araújo Alves <kayromaraujo067@gmail.com>
2026-07-09 21:00:34 -0300

    a
    a

---
+++ append.py
@@ -0,0 +1,12 @@
+lista = ['user1', 'user2', 'user3']
+
+lista.remove('user1')
+
+print(lista)
+
+lista.append('User0')
+
+
+# Aprendendo a usar um pouco de Append e Remove
+# Append = Adiciona algum conteúdo a mais para Variável/Lista
+# Remove = Remove algum conteúdo da Variável/Lista
