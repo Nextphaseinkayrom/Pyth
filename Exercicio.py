5b717eae799b8994799cca047decaec23ff888d3
Kayrom Araújo Alves <kayromaraujo067@gmail.com>
2026-07-15 19:46:15 -0300

    a
    a

---
+++ Exercicio.py
@@ -0,0 +1,9 @@
+# Diria que o comando Splict na minha interpretação
+# Ele Junta várias input de uma vez, otimiza-as
+
+
+print('Olá user ???')
+
+nome, idade, cidade = input('Digite seu nome, idade e cidade:). . .').split(",")
+
+print(f"Olá {nome}.Seja bem vindo, vimos que sua idade é de{idade} Anos e Sua cidade é{cidade}")
