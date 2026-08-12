d0406092782edf7b85b3c74e510b9510c8fc6298
NextCommit <kayromaraujo067@gmail.com>
2026-08-12 18:29:46 -0300

    a
    a

--- Nivel2/projeto.py
+++ Nivel2/projeto.py
@@ -1,21 +1,43 @@
 pessoas = []
 class Person:
-  def __init__(self, nome, vida, idade):
-    self.nome = nome
+  def __init__(self, name, vida, idade):
+    self.name = name
     self.vida = vida
     self.idade = idade
+
   def infor(self):
-    return (f'Nome: {self.nome} | Vida: {self.vida} | Idade: {self.idade}')
-for i in range(3):
+    return f'Nome: {self.name} | Vida: {self.vida} | Idade: {self.idade}'
 
-  perg_1 = str(input('Nome: '))
-  perg_2 = int(input('Vida: '))
-  perg_3 = int(input('Idade: '))
-  p_1 = Person(perg_1, perg_2, perg_3)
-  pessoas.append(p_1)
 
-for pessoa in pessoas:
- print(pessoa.infor())
+while True:
+ print("""
+ 1 - Ver cadastros
+ 2 - Fazer cadastros
+ 3 - Sair""")
+ pergunta = int(input('digite oque você deseja'))
+
+ if pergunta == 1:
+    print(f'Os cadastrados no momento são. . .\n')
+    for pessoas in pessoas:
+        print(pessoas.infor())
+
+
+
+ elif pergunta == 2:
+    print('Indo ao registro')
+    perg_1 = str(input('Name: '))
+    perg_2 = int(input('Vida: '))
+    perg_3 = int(input('Idade: '))
+
+    pessoa = Person(perg_1, perg_2, perg_3)
+    pessoas.append(pessoa)
+
+    for pessoa in pessoas:
+     print(pessoa.infor())
+ else:
+    print('Saindo')
+    break
+
 
 
 
