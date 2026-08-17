ea84adaa56efaad0789a3c8123341d504563540c
NextCommit <kayromaraujo067@gmail.com>
2026-08-17 18:04:02 -0300

    a
    a

---
+++ ProjetoP.C/arquivo_2.py
@@ -0,0 +1,18 @@
+def pessoas(**kwargs):
+    return kwargs
+pessoa1={
+            "Nome": "João",
+            "Saldo": 340,
+            "Senha": 1032
+},
+pessoa2={
+            "Nome": "Dayane",
+            "Saldo": 200,
+            "Senha": 9203
+        }
+
+
+
+        #TENTAR RESOLVER O ERRO DOIMPORT QUE NÃO É DE DEUS
+
+
